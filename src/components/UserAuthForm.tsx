a3cdeb5260b63bb9de6730518dde16c410b904e8
Ankita Goyal <goyalankita0325@gmail.com>
2025-02-10 10:52:17 +0530

    update

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Button } from "./ui/Button";
+
+interface UserAuthFormProps {}
+
+const UserAuthForm : FC<UserAuthFormProps> = ({}) => {
+    return <Button>Goggle</Button>
+}
+
+export default UserAuthForm;
